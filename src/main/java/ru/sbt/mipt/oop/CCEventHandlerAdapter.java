0726e47ff8f55bf8f6f03d343c2e87c7efc3263e
Eremenko Ilya <TheNeonLightning@yandex.ru>
2021-05-06 13:24:29 +0300

    Deleting unused method adaptEventType()

--- src/main/java/ru/sbt/mipt/oop/CCEventHandlerAdapter.java
+++ src/main/java/ru/sbt/mipt/oop/CCEventHandlerAdapter.java
@@ -27,21 +27,4 @@ public class CCEventHandlerAdapter implements com.coolcompany.smarthome.events.E
                 event.getObjectId()
         );
     }
-
-    /**
-     * Available event types in library v3.7.1:
-     *    "LightIsOn", "LightIsOff", "DoorIsOpen", "DoorIsClosed",
-     *    "DoorIsLocked", "DoorIsUnlocked"
-     */
-    private SensorEventType adaptEventType(String eventType) {
-        return switch (eventType) {
-            case ("LightIsOn") -> SensorEventType.LIGHT_ON;
-            case ("LightIsOff") -> SensorEventType.LIGHT_OFF;
-            case ("DoorIsOpen") -> SensorEventType.DOOR_OPEN;
-            case ("DoorIsClosed") -> SensorEventType.DOOR_CLOSED;
-            case ("DoorIsLocked") -> SensorEventType.DOOR_LOCKED;
-            case ("DoorIsUnlocked") -> SensorEventType.DOOR_UNLOCKED;
-            default -> throw new IllegalStateException("Unexpected value: " + eventType);
-        };
-    }
 }
