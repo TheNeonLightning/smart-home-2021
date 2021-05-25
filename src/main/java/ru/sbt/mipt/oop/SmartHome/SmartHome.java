9b95daeeb4ccee5ae7794bbe1f52a5ad189ae97f
Eremenko Ilya <TheNeonLightning@yandex.ru>
2021-05-25 13:14:28 +0300

    Git does not see hometask-2 version of SmartHome class so have to update it here

--- src/main/java/ru/sbt/mipt/oop/SmartHome/SmartHome.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHome/SmartHome.java
@@ -3,8 +3,16 @@ package ru.sbt.mipt.oop.SmartHome;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class SmartHome {
+public class SmartHome implements Actionable {
     Collection<Room> rooms;
+
+    @Override
+    public void execute(Action action) {
+        for (Room room : rooms) {
+            room.execute(action);
+        }
+        action.applyAction(this);
+    }
 
     public SmartHome() {
         rooms = new ArrayList<>();
@@ -12,13 +20,5 @@ public class SmartHome {
 
     public SmartHome(Collection<Room> rooms) {
         this.rooms = rooms;
-    }
-
-    public void addRoom(Room room) {
-        rooms.add(room);
-    }
-
-    public Collection<Room> getRooms() {
-        return rooms;
     }
 }
